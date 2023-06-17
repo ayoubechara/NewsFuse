0e1989e165780aed27a93d8c6f2771e737f8c09c
ekyrius <ayoub_echara@outlook.com>
2023-06-17 14:02:03 +0100

    newsapi update

--- backend/app/Services/NewsAPI.php
+++ backend/app/Services/NewsAPI.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use DateTime;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
@@ -24,17 +25,20 @@ class NewsAPI
     public function getArticles($keyword, $category, $date, $limit)
     {
         if (empty($keyword) && empty($category)) {
-            $response = Http::get('https://newsapi.org/v2/top-headlines', [
+            $response = Http::get('https://newsapi.org/v2/everything', [
                 'sources' => 'abc-news,associated-press,bbc-news,bloomberg,cnn,espn,financial-times,fox-news,google-news,nbc-news,the-new-york-times,reuters,the-guardian,the-washington-post,usa-today',
+                'from' => $date,
                 'pageSize' => $limit,
+                'language' => 'en',
                 'apiKey' => $this->apiKey,
             ]);
         } else {
             $response = Http::get('https://newsapi.org/v2/top-headlines', [
                 'q' => $keyword,
-                'category' => strtolower($category),
+                'category' => $category,
                 'from' => $date,
                 'pageSize' => $limit,
+                'language' => 'en',
                 'apiKey' => $this->apiKey,
             ]);
         }
@@ -43,25 +47,32 @@ class NewsAPI
         $articles = [];
         if (isset($data['articles'])) {
             foreach ($data['articles'] as $article) {
-                $title = $article['title'];
-                $description = $article['description'];
-                $source_name = $article['source']['name'];
-                $author = $article['author'];
-                $publishing_date = $article['publishedAt'];
-                $url = $article['url'];
-                $image_url = $article['urlToImage'];
 
-                $article_obj = [
-                    'title' => $title,
-                    'description' => $description,
-                    'source_name' => $source_name,
-                    'author' => $author,
-                    'publishing_date' => $publishing_date,
-                    'url' => $url,
-                    'image_url' => $image_url
-                ];
 
-                $articles[] = $article_obj;
+                $datetime1 = new DateTime($date);
+                $datetime2 = new DateTime($article['publishedAt']);
+
+                if (!$date || $datetime1->format('Y-m-d') === $datetime2->format('Y-m-d')) {
+                    $title = $article['title'];
+                    $description = $article['description'];
+                    $source_name = $article['source']['name'];
+                    $author = $article['author'];
+                    $publishing_date = $article['publishedAt'];
+                    $url = $article['url'];
+                    $image_url = $article['urlToImage'];
+
+                    $article_obj = [
+                        'title' => $title,
+                        'description' => $description,
+                        'source_name' => $source_name,
+                        'author' => $author,
+                        'publishing_date' => $publishing_date,
+                        'url' => $url,
+                        'image_url' => $image_url
+                    ];
+
+                    $articles[] = $article_obj;
+                }
             }
         }
 
